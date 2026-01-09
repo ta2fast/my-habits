62a0a62f30ac68f03aeebee6222c06f7c56003f1
ta2fast <takayukitaniguchi04@gmail.com>
2026-01-10 00:05:14 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,14 @@
+const CACHE_NAME = 'habit-tracker-v1';
+const ASSETS = [
+    'index.html',
+    'manifest.json',
+    'icon.png'
+];
+
+self.addEventListener('install', (e) => {
+    e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+});
+
+self.addEventListener('fetch', (e) => {
+    e.respondWith(caches.match(e.request).then(response => response || fetch(e.request)));
+});
